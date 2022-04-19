8442b4a7b317ff3fb17b1783b5873db591118f71
Emil Kucharczyk <czerepaszka.it@gmail.com>
2022-04-19 23:46:38 +0200

    Added Target Endpoint class

--- WakeDevice/Program.cs
+++ WakeDevice/Program.cs
@@ -1,5 +1,27 @@
-﻿//Get command parser input
-//Null check crucial arguments
+﻿using WakeDevice;
+
+try
+{
+    Parsing Command = new(args);
+    if (Command.AddressMac == null || Command.AddressIp == null)
+    {
+        return;
+    }
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Fatal error occured.");
+
+#if DEBUG 
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.StackTrace);
+#endif
+
+    return;
+}
+
+
 //Build target object
 //Build magic packet
 //Perform device wake
---
+++ WakeDevice/TargetEndpoint.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.NetworkInformation;
+namespace WakeDevice
+{
+    internal class TargetEndpoint
+    {
+        #region Fields
+        private IPAddress _addressIP;
+        private IPAddress _addressMask = IPAddress.Parse("255.255.255.0");
+        private List<uint> _addressPorts = new() { 7, 9 };
+        private PhysicalAddress _mac;
+        #endregion;
+
+        #region Properties
+        public IPAddress AddressIP
+        {
+            get
+            {
+                return _addressIP;
+            }
+            private set
+            {
+                if (value != null)
+                {
+                    _addressIP = value;
+                }
+            }
+        }
+        public IPAddress? AddressMask
+        {
+            get
+            {
+                return _addressMask;
+            }
+            private set
+            {
+                if (value != null)
+                {
+                    _addressMask = value;
+                }
+            }
+        }
+        public List<uint>? AddressPorts
+        {
+            get
+            {
+                return _addressPorts;
+            }
+            private set
+            {
+                if (value != null)
+                {
+                    _addressPorts = value;
+                }
+            }
+        }
+        public PhysicalAddress Mac
+        {
+            get
+            {
+                return _mac;
+            }
+            private set
+            {
+                if (value != null)
+                {
+                    _mac = value;
+                }
+            }
+        }
+        #endregion;
+
+        public TargetEndpoint(string mac, string ip, string? mask, List<uint>? ports)
+        {
+
+            if (!PhysicalAddress.TryParse(mac, out PhysicalAddress parsedMac))
+            {
+                throw new ArgumentException("Invalid MAC address has been specified.");
+            }
+            Mac = parsedMac;
+
+            if (!IPAddress.TryParse(ip, out IPAddress parsedIp))
+            {
+                throw new ArgumentException("Invalid IP address has been specified.");
+            }
+            AddressIP = parsedIp;
+
+            if (!IPAddress.TryParse(mask, out IPAddress parsedMask))
+            {
+                throw new ArgumentException("Invalid subnet mask has been specified.");
+            }
+            AddressMask = parsedMask;
+
+            AddressPorts = ports;
+        }
+    }
+}
